d40f8058d2115588a7b140a4133b08a3ac8aa1f3
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-04-11 13:48:42 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,31 +1,31 @@
 #
-# Cookbook:: cookbook
+# Cookbook:: cookboo'
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
-execute ‘install’ do
-     command ‘apt-get install git devscripts debhelper build-essential dh-make’
+execute 'install' do
+     command 'apt-get install git devscripts debhelper build-essential dh-make'
 end
 
-execute ‘clone’ do
-      command ‘git clone https://github.com/spotify/docker-gc.git'
+execute 'clone' do
+      command 'git clone https://github.com/spotify/docker-gc.git'
 end
 
-execute ‘move’ do
-       command ‘cd docker-gc’
+execute 'move' do
+       command 'cd docker-gc'
 end
 
-execute ‘build’ do
-     command ‘debuild -us -uc -b’
+execute 'build' do
+     command 'debuild -us -uc -b'
 end
 
-dpkg_package "docker-gc” do
-  source “docker-gc_0.1.0_all.deb"
+dpkg_package 'docker-gc' do
+  source 'docker-gc_0.1.0_all.deb'
   action :install
 end
 
-cron “docker-gc” do
+cron 'docker-gc' do
   minute '0'
   hour '*/1'
-  command "/usr/sbin/docker-gc"
+  command '/usr/sbin/docker-gc'
   action :create
