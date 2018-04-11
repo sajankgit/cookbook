3bfaa074d338b9f3991a55e64869c19cb3ab6c21
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-04-11 17:34:01 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,18 +15,6 @@ execute 'clone' do
       command 'sudo git clone https://github.com/spotify/docker-gc.git'
 end
 
-execute 'update' do
-     command 'sudo apt-get update && apt-get upgrade'
-end
-
-execute 'install' do
-     command 'sudo apt-get -y install git devscripts debhelper build-essential dh-make'
-end
-
-execute 'clone' do
-      command 'sudo git clone https://github.com/spotify/docker-gc.git'
-end
-
 bash 'move' do
   cwd '/docker-gc'
   code <<-EOH
