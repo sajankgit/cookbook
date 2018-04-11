9ce484936d3cedad8540a04e7aa2ff418381eeb2
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-04-11 13:18:45 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,14 @@
 # Recipe:: default
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
+dpkg_package "docker-gc” do
+  source "/tmp/docker-gc_0.1.0_all.deb"
+  action :install
+end
+
+
+cron “docker-gc” do
+  minute '0'
+  hour '*/1'
+  command "/usr/sbin/docker-gc"
+  action :create
