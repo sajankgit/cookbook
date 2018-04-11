20f5ff58366ce7a538ebcaa0fd5bf0c14472e910
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-04-11 16:48:39 +0530

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,23 +15,4 @@ execute 'clone' do
       command 'sudo git clone https://github.com/spotify/docker-gc.git'
 end
 
-execute 'move' do
-    cwd "docker-gc"
-     
-end
 
-execute 'build' do
-     command 'sudo debuild -us -uc -b'
-end
-
-dpkg_package 'docker-gc' do
-  source 'docker-gc_0.1.0_all.deb'
-  action :install
-end
-
-cron 'docker-gc' do
-  minute '0'
-  hour '*/1'
-  command '/usr/sbin/docker-gc'
-  action :create
-end
