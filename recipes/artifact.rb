d0626c06b164ef81cd0222d830a116d3cebcf42d
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-09 17:20:33 +0530

    Update artifact.rb

--- recipes/artifact.rb
+++ recipes/artifact.rb
@@ -7,30 +7,3 @@ execute 'update' do
      command 'sudo apt-get update'
 end
 
-bash ‘install_java’ do
- cwd '/root'
- code <<-EOH
- add-apt-repository ppa:webupd8team/java
- apt-get update
- apt-get -y install oracle-java8-installer
- EOH
-end
-
-bash ‘install_nginx’ do
- code <<-EOH
- wget -c -O- http://nginx.org/keys/nginx_signing.key | sudo apt-key add -
- echo "deb http://nginx.org/packages/ubuntu/ trusty nginx" | sudo tee -a /etc/apt/sources.list.d/nginx.list > /dev/null
- apt-get update
- apt-get -y install nginx
- service nginx restart
- EOH
-end
-
-bash ‘install_jfrog’ do
- code <<-EOH
- wget -c -O- "https://bintray.com/user/downloadSubjectPublicKey?username=jfrog" | sudo apt-key add -
- echo "deb https://jfrog.bintray.com/artifactory-pro-debs trusty main" | sudo tee -a /etc/apt/sources.list
- apt-get update
- apt-get install jfrog-artifactory-pro
- EOH
-end
