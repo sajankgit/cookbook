c36719cb89b1f51fc75de8f8994779c0c71066b3
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-09 15:25:35 +0530

    Update artifact.rb

--- recipes/artifact.rb
+++ recipes/artifact.rb
@@ -5,8 +5,14 @@ end
 bash ‘install’ do
  code <<-EOH
  wget -c -O- "https://bintray.com/user/downloadSubjectPublicKey?username=jfrog" | sudo apt-key add -
+ EOH
+ code <<-EOH
  echo "deb https://jfrog.bintray.com/artifactory-pro-debs trusty main" | sudo tee -a /etc/apt/sources.list
+ EOH
+ code <<-EOH
  apt-get update
+ EOH
+ code <<-EOH 
  apt-get install jfrog-artifactory-pro
  EOH
 end
