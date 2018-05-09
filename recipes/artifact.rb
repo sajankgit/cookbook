4f9e60b87251ba0f1f375d56138d7e12325d4411
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-09 22:09:28 +0530

    Update artifact.rb

--- recipes/artifact.rb
+++ recipes/artifact.rb
@@ -11,6 +11,6 @@ bash 'install_java' do
  code <<-EOH
  add-apt-repository ppa:webupd8team/java
  apt-get update
- apt-get DEBIAN_FRONTEND=noninteractive install -y oracle-java8-installer
+ apt-get install -y oracle-java8-installer
  EOH
 end
