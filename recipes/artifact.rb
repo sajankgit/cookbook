1f54d03f3b1b7a069db614782821b037b7ac9a64
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-09 17:44:10 +0530

    Update artifact.rb

--- recipes/artifact.rb
+++ recipes/artifact.rb
@@ -7,14 +7,13 @@ execute 'update' do
      command 'sudo apt-get update'
 end
 
-execute ‘repo’ do
-     command 'add-apt-repository ppa:webupd8team/java'
+include_recipe 'nginx::repo'
+ 
+package 'nginx' do
+  action :install
 end
 
-execute 'update' do
-     command 'sudo apt-get update'
-end
-
-execute ‘install’ do
-     command 'apt-get -y install oracle-java8-installer'
+service 'nginx' do
+  supports status: true, restart: true, reload: true
+  action :enable
 end
