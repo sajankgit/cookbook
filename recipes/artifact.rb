ab4f3d4098a28f03b58a6e14921f768c5c70cc2f
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-09 13:49:08 +0530

    Update artifact.rb

--- recipes/artifact.rb
+++ recipes/artifact.rb
@@ -7,3 +7,11 @@ bash ‘installjava’ do
  add-apt-repository ppa:webupd8team/java
  EOH
 end
+
+execute 'update' do
+     command 'sudo apt-get update'
+end
+
+execute 'install' do
+     command 'apt-get -y install oracle-java8-installer'
+end
