9fcef0283563dae5f81dfb7011ce369df65765be
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-10 09:29:59 +0530

    Update artifact.rb

--- attributes/artifact.rb
+++ attributes/artifact.rb
@@ -1,6 +1,14 @@
-
-default['java']['jdk_version'] = '8'
-default['java']['install_flavor'] = 'oracle'
-default['java']['jdk']['7']['x86_64']['url'] = 'http://artifactory.example.com/artifacts/jdk-7u65-linux-x64.tar.gz'
-default['java']['jdk']['7']['x86_64']['checksum'] = 'The SHA-256 checksum of the JDK archive'
-default['java']['oracle']['accept_oracle_download_terms'] = true
+name "java"
+description "Install Oracle Java"
+default_attributes(
+  "java" => {
+    "install_flavor" => "oracle",
+    "jdk_version" => "8",
+    "oracle" => {
+      "accept_oracle_download_terms" => true
+    }
+  }
+)
+run_list(
+  "recipe[artifact]"
+)
