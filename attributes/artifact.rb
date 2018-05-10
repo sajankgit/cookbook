8d277f0eee24ea42c12c9410a784ee1174f8b0e8
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-10 09:19:38 +0530

    Update artifact.rb

--- attributes/artifact.rb
+++ attributes/artifact.rb
@@ -1,13 +1,6 @@
-{
-  "name": "java",
-  "description": "Oracle java role",
-  "override_attributes": {
-    "java": {
-      "jdk_version": 8,
-      "install_flavor": "oracle",
-      "oracle": {
-        "accept_oracle_download_terms": true
-      }
-    }
-  }
-}
+
+default['java']['jdk_version'] = '8'
+default['java']['install_flavor'] = 'oracle'
+default['java']['jdk']['7']['x86_64']['url'] = 'http://artifactory.example.com/artifacts/jdk-7u65-linux-x64.tar.gz'
+default['java']['jdk']['7']['x86_64']['checksum'] = 'The SHA-256 checksum of the JDK archive'
+default['java']['oracle']['accept_oracle_download_terms'] = true
