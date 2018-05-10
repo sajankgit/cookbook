5348c1099e0b8a76c3faff342652f2d4a5eb997e
sajankgit <37524392+sajankgit@users.noreply.github.com>
2018-05-10 09:00:31 +0530

    Update artifact.rb

--- recipes/artifact.rb
+++ recipes/artifact.rb
@@ -7,4 +7,10 @@ execute 'update' do
      command 'sudo apt-get update'
 end
 
-include_recipe mycookbooks::default
+bash 'install_java' do
+ code <<-EOH
+ add-apt-repository ppa:webupd8team/java
+ apt-get update
+ apt-get -y install oracle-java8-installer
+ EOH
+end
